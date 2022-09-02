a7238ebdee4740b0173c13a478f24bd429c04768
Hetvi2512 <hetvishah2512@gmail.com>
2022-09-02 15:26:24 +0530

    Added anchor tag

--- components/HomePage.js
+++ components/HomePage.js
@@ -7,8 +7,7 @@ function HomePage({ resp, combinedData1, combinedData2 }) {
   return (
     <>
       <div className="common-section">
-      <div className="common-partone">
-        </div>
+        <div className="common-partone"></div>
         <div className="common-parttwo">
           <div className="common-main-div">
             {" "}
@@ -17,33 +16,41 @@ function HomePage({ resp, combinedData1, combinedData2 }) {
                 className={`common-main-${index + 1} d-flex  flex-column`}
                 key={a.id}
               >
-                <Link href={`/${a["story"]["slug"]}`} prefetch={false}  passHref={true}>
-                  <div style={{ cursor: "pointer" }}>
-                    <img
-                      src={`${imgBaseURL}${a["story"]["hero-image-s3-key"]}`}
-                      alt=""
-                      // width={150}
-                      // height={150}
-                    />
-                    <Link
-                      href={
-                        subCatPageUrl[a["story"]["sections"][0]["slug"]].url
-                      }
-                      prefetch={false}
-                      passHref={true}
-                    >
-                      <div className="Biotif-Book common-category">
-                        {a["story"]["sections"][0]["name"].toUpperCase()}
+                <Link
+                  href={`/${a["story"]["slug"]}`}
+                  prefetch={false}
+                  passHref={true}
+                >
+                  <a>
+                    <div style={{ cursor: "pointer" }}>
+                      <img
+                        src={`${imgBaseURL}${a["story"]["hero-image-s3-key"]}`}
+                        alt=""
+                        // width={150}
+                        // height={150}
+                      />
+                      <Link
+                        href={
+                          subCatPageUrl[a["story"]["sections"][0]["slug"]].url
+                        }
+                        prefetch={false}
+                        passHref={true}
+                      >
+                        <a>
+                          <div className="Biotif-Book common-category">
+                            {a["story"]["sections"][0]["name"].toUpperCase()}
+                          </div>
+                        </a>
+                      </Link>
+                      <div className="common-main-1-text InterstateCompressed-Bold">
+                        {/* THE COMPLETE GUIDE TO NFTS */}
+                        {a["story"]["headline"].toUpperCase()}
                       </div>
-                    </Link>
-                    <div className="common-main-1-text InterstateCompressed-Bold">
-                      {/* THE COMPLETE GUIDE TO NFTS */}
-                      {a["story"]["headline"].toUpperCase()}
+                      <div className="common-author Biotif-Book">
+                        By {a["story"]["author-name"]}{" "}
+                      </div>
                     </div>
-                    <div className="common-author Biotif-Book">
-                      By {a["story"]["author-name"]}{" "}
-                    </div>
-                  </div>
+                  </a>
                 </Link>
                 <hr />
               </div>
@@ -51,7 +58,7 @@ function HomePage({ resp, combinedData1, combinedData2 }) {
           </div>
         </div>
       </div>
-      <TickerComp resp={combinedData1['breaking-news']} />
+      <TickerComp resp={combinedData1["breaking-news"]} />
       <div style={{ height: "500px" }}>
         <h3>HEIGHT</h3>
       </div>
